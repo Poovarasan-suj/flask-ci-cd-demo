9e1c7118b40b67218118395277dba0f301404c4c
Poovarasan-suj <epoovarasansujith25@gmail.com>
2026-04-10 08:46:42 +0530

    Initialize Flask app with basic routes

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins CI/CD Pipeline 🚀"
+
+@app.route("/env")
+def env():
+    return f"Environment: {os.getenv('ENV', 'Not Set')}"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
